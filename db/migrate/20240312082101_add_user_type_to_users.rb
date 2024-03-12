0be532a37c1e2362c2ec36a04d8377424a36e2a3
Muhammad Saleem <muhammad.saleem@transreport.co.uk>
2024-03-12 13:26:24 +0500

    image display

---
+++ db/migrate/20240312082101_add_user_type_to_users.rb
@@ -0,0 +1,5 @@
+class AddUserTypeToUsers < ActiveRecord::Migration[7.1]
+  def change
+    add_column :users, :user_type, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_11_125905) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_12_082101) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -97,7 +97,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_11_125905) do
     t.string "password"
     t.string "username"
     t.string "status"
-    t.string "user_type"
+    t.string "type"
     t.string "profession"
     t.string "service"
     t.datetime "created_at", null: false
@@ -106,6 +106,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_11_125905) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "user_type"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
