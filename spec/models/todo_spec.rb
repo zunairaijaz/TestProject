f53eea608e4e2c28268488dd98ebc7d737266583
Muhammad Saleem <muhammad.saleem@transreport.co.uk>
2024-03-14 16:27:36 +0500

    todo validation/associations test

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe 'validations' do
+    it {should validate_presence_of(:title)}
+    it {should validate_presence_of(:description)}
+end
+describe 'associations' do
+    it {should belong_to(:campaign)}
+end
+
 end
